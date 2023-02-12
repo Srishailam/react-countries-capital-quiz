2ec3034a58510369104e0cc1d59eb359d933bf6b
Srishailam <sriyd531@gmail.com>
2023-02-11 22:34:27 -0500

    continents filtering

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,26 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [continets, setContinets] = useState([]);
+  const [selectedContinets, setSelectedContinets] = useState([]);
 
   useEffect(() => {
     setIsLoading(true)
     fetch('https://restcountries.com/v3.1/all')
       .then(response => response.json())
       .then(data => {
-        const countries = data.map(country => country);
+        const countries = data.filter(country => country.independent && country.unMember);
+        const continentsData = countries.map(country => country?.continents?.[0])
+        setContinets([...new Set([...continentsData])].sort())
         setCountries(countries);
         setIsLoading(false);
       });
   }, []);
 
   const sendRequest = () => {
-    const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(value.toLowerCase()));
-    setFilteredCountries(filteredCountries);
+    const filteredCountriesByQuery = countries.filter(country => country.name.common.toLowerCase().includes(value.toLowerCase()));
+    const filteredCountriesByContinents = filteredCountriesByQuery.filter(country => selectedContinets.length === 0 ? true : selectedContinets.includes(country.continents[0]))
+    setFilteredCountries(filteredCountriesByContinents);
   }
 
   const ref = useRef(sendRequest);
@@ -44,6 +49,21 @@ function App() {
     debouncedCallback();
   };
 
+  const handleChange = e => {
+    const {value, name, checked} = e.target;
+
+    if(checked){
+      setSelectedContinets([...selectedContinets, value]);
+    } else {
+      setSelectedContinets([...selectedContinets.filter( e => e !== value)])
+    }
+  };
+
+  useEffect(() => {
+    sendRequest()
+  }, [selectedContinets])
+
+
   if(isLoading){
     return 'Loading....'
   }
@@ -53,12 +73,27 @@ function App() {
     <div className="App">
       <div className="App-container">
         <input className='App-input' type="text" onChange={onChange} value={value} placeholder="Search for a country name..."/>
+        <div className='App-filters'>
+          {
+            continets.map(c => {
+              return (
+                <p key={c}>
+                  <input type="checkbox" id={c} onChange={handleChange} value={c} name={c}/>
+                  <label htmlFor={c}>{c}</label>
+                </p>
+              )
+            })
+          }
+          {
+            <p className='count'>{`Total:${filteredCountries.length}`}</p>
+          }
+        </div>
         <div className='App-results'>
           <ul>
             {filteredCountries.map(country => {
               const { flags, name } = country;
               return (
-                <li key={country}>
+                <li key={country.name.common}>
                   <a>
                     <img src={flags.png} alt={flags.alt} />
                     <h1>{name.common}</h1>
